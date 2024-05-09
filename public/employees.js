29b6069cfa5c2276bb34eedf46b2927a099fe613
jaskiratnandhra <jaskiratnandhra@gmail.com>
2024-05-09 02:23:40 -0700

    M13 Lab 1

---
+++ public/employees.js
@@ -0,0 +1,15 @@
+let jaskirat = "Jaskirat Singh";
+let Saurav = "Saurav Nair";
+let Sukrita = "Sukrita Kumar";
+let Rebeeca = "Rebeeca Robin";
+let Rocky = "Rocky Road";
+let The = "The Rock";
+let Kevin = "Kevin Hart";
+let Biran = "Biran Bill";
+const element = /*#__PURE__*/React.createElement("ul", {
+  style: {
+    color: "blue",
+    fontSize: "24px"
+  }
+}, /*#__PURE__*/React.createElement("li", null, jaskirat), /*#__PURE__*/React.createElement("li", null, Saurav), /*#__PURE__*/React.createElement("li", null, Sukrita), /*#__PURE__*/React.createElement("li", null, Rebeeca), /*#__PURE__*/React.createElement("li", null, Rocky), /*#__PURE__*/React.createElement("li", null, The), /*#__PURE__*/React.createElement("li", null, Kevin), /*#__PURE__*/React.createElement("li", null, Biran));
+ReactDOM.render(element, document.getElementById("content"));
---
+++ src/employees.jsx
@@ -0,0 +1,21 @@
+let jaskirat = "Jaskirat Singh";
+let Saurav = "Saurav Nair";
+let Sukrita = "Sukrita Kumar";
+let Rebeeca = "Rebeeca Robin";
+let Rocky = "Rocky Road";
+let The = "The Rock";
+let Kevin = "Kevin Hart";
+let Biran = "Biran Bill";
+const element = (
+  <ul style={{ color: "blue", fontSize: "24px" }}>
+    <li>{jaskirat}</li>
+    <li>{Saurav}</li>
+    <li>{Sukrita}</li>
+    <li>{Rebeeca}</li>
+    <li>{Rocky}</li>
+    <li>{The}</li>
+    <li>{Kevin}</li>
+    <li>{Biran}</li>
+  </ul>
+);
+ReactDOM.render(element, document.getElementById("content"));
